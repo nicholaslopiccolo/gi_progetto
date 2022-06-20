972f41453edfb1c4997708c4daca24f5c6df3fa2
nicholas lo piccolo <giokkaaaa@gmail.com>
2022-06-20 20:58:55 +0200

    feat: aggiunta webcrawler

--- src/querysystem.py
+++ src/querysystem.py
@@ -5,6 +5,14 @@ query richieste dall'utente.
 Whoosh
 https://whoosh.readthedocs.io/en/latest/quickstart.html
 """
-
+from whoosh import index
 class QuerySystem:
-    pass
+    def __init__(self):
+        self.queries = []
+
+    def search(self):
+        q = QuerySystem.query(input("Type your query: "))
+        self.queries.append(q)
+    class query:
+        def __init__(self):
+            self.str = str
--- src/webcrawler.py
+++ src/webcrawler.py
@@ -1,66 +1,212 @@
+from operator import index
 from bs4 import BeautifulSoup
-import requests, re
+import requests, re, os
 
-regex = ("((http|https)://)(www.)?" +
+url_regex = ("((http|https)://)(www.)?" +
              "[a-zA-Z0-9@:%._\\+~#?&//=]" +
              "{2,256}\\.[a-z]" +
              "{2,6}\\b([-a-zA-Z0-9@:%" +
              "._\\+~#?&//=]*)")
 
-class WebCrawler:
-    def __init__(self,initial_url:str,corpus_path:str,limit:int = None) -> None:
-        self.INITIAL_URL = initial_url
-        self.URL_REGEX = re.compile(regex)
-        self.LIMIT = limit | 300
-        self.CORPUS_PATH = corpus_path
+domain_regex = ('https?://([A-Za-z_0-9.-]+).*')
 
-        self.counter = 0
+class WebCrawler:
+    URL_PATTERN = re.compile(url_regex)
+    DOMAIN_PATTERN = re.compile(domain_regex)
+
+    def __init__(self,limit=300):
+        self.LIMIT = limit
+        self.CORPUS_PATH = "./Docs"
         self.links = []
-    
-    # TO DO
-    def create_document(self,soup,url) -> None: 
+
+
+    @staticmethod
+    def get_domain(url):
+        return WebCrawler.DOMAIN_PATTERN.search(url).group(1)
+
+    def create_document_meetup(self,soup,url,index): 
         # Crea il documento sfruttanzo la soup (bisogna cercare il contenuto)
         # Il nome del file dev'essere collegato al suo url
-        pass
+        d = "{0}/{1}".format(self.CORPUS_PATH,self.INITIAL_DOMAIN)
+        file_name = "{0}/{1}.txt".format(d,index)
+        if not os.path.exists(d):
+            os.mkdir(d)
 
-    def run(self) -> None: 
+        with open(file_name, 'w',encoding='utf-8') as f:
+            f.write(url + "\n")
+            try:
+                f.write(soup.title.string +"\n")
+            except AttributeError:
+                pass
+            
+            try:
+                f.write(soup.select_one('div[data-testid="event-rsvp-time"]').get_text()+"\n\n")
+            except AttributeError:
+                f.write('--no date--\n\n')
+            try:
+                f.write(soup.select_one('div[data-event-label="body"]').get_text())
+            except AttributeError:
+                f.write('--no body--\n\n')
+
+            print("\nfile created: {0} \nfor the url: {1}".format(file_name,url))
+
+    def create_document_eventbrite(self,soup,url,index): 
+        # Crea il documento sfruttanzo la soup (bisogna cercare il contenuto)
+        # Il nome del file dev'essere collegato al suo url
+        d = "{0}/{1}".format(self.CORPUS_PATH,self.INITIAL_DOMAIN)
+        file_name = "{0}/{1}.txt".format(d,index)
+        if not os.path.exists(d):
+            os.mkdir(d)
+
+        with open(file_name, 'w',encoding='utf-8') as f:
+            f.write(url + "\n")
+            try:
+                f.write(soup.title.string +"\n")
+            except AttributeError:
+                pass
+            
+            divs = soup.find_all('div',{'class':'event-details__data'})
+            try:
+                data = ', '.join([text for text in divs[0].stripped_strings][:-1])
+                f.write(data+"\n")
+            except Exception:
+                f.write('--no date--\n\n')
+            finally:
+                try:
+                    luogo = ', '.join([text for text in divs[1].stripped_strings][:-2])
+                    f.write(luogo+"\n")
+                except Exception:
+                    f.write('--no location--\n\n')
+                finally:
+                    f.write('\n')
+
+
+            try:
+                # Rimozione dati inutili riguardanti date e luogo
+                [div.decompose() for div in soup.find_all('div',{'class':'event-details__data'})]
+                div = soup.find_all('div',{'class':'listing-info__body'})[0]
+                f.write('\n'.join([text for text in div.stripped_strings]))
+            except Exception:
+                f.write('--no body--\n\n')
+
+            print("\nfile created: {0} \nfor the url: {1}".format(file_name,url))
+
+    def run_meetup(self,initial_url="https://www.meetup.com"): 
         """
         La funziona lancia il crawler che scava nel dominio alla ricerca di:
         contenuto e altri link a cui agganciarsi per le prossime richieste
         """
+        self.INITIAL_URL = initial_url
+        self.INITIAL_DOMAIN = WebCrawler.get_domain(self.INITIAL_URL)
+
         html = requests.get(self.INITIAL_URL).text
         self.links.append(self.INITIAL_URL)
-        self.counter += 1
+        index = 0
 
         # Esegue il crawling di 300 siti link
-        while self.counter < self.LIMIT-1:
+        while len(self.links) < self.LIMIT and index < len(self.links):
             soup = BeautifulSoup(html, 'html.parser')
             a_list = soup.find_all("a")
 
             # Carica solo url corretti
-            new_links = [link.get('href') for link in a_list if(re.search(self.URL_REGEX, link.get('href')))]
+            try: 
+                support = { link.get('href') for link in a_list if link.get('href') and re.search(self.URL_PATTERN, link.get('href')) and re.search("/events/",link.get('href')) }
+            except TypeError:
+                continue
+            new_links = { link for link in support if WebCrawler.get_domain(link) == self.INITIAL_DOMAIN }
             self.links += new_links
 
-            url = self.links[self.counter]
-            # Create file
-            self.create_document(soup,url)
+            url = self.links[index]
+            
 
             print("Crawling: {0}".format(url))
             print("  Found: {0}".format(len(new_links)))
             print("  Total: {0}".format(len(self.links)))
 
-            self.counter += 1
+            index += 1
 
             # Un generico errore durante richiesta
             try:
                 html = requests.get(url).text
-            except Exception as e:
-                print(e)
+            except Exception:
+                pass
 
-def test():
-    INITIAL_URL = "https://www.google.it"
+        # Crea i documenti dalla source
+        index = 0
+        for url in self.links:
+            try:
+                html = requests.get(url).text
+            except Exception:
+                continue
 
-    wc = WebCrawler(initial_url=INITIAL_URL, corpus_path="../files/", limit=10)
-    wc.run()
+            soup = BeautifulSoup(html, 'html.parser')
+            self.create_document_meetup(soup,url,index)
 
-#test()
+            index +=1
+
+
+    def run_eventbrite(self,initial_url="https://www.eventbrite.it/d/italy--carpi-centro/bologna/?page=1"): 
+        """
+        La funziona lancia il crawler che scava nel dominio alla ricerca di:
+        contenuto e altri link a cui agganciarsi per le prossime richieste
+        """
+        self.INITIAL_URL = initial_url
+        self.INITIAL_DOMAIN = WebCrawler.get_domain(self.INITIAL_URL)
+
+        html = requests.get(self.INITIAL_URL).text
+        self.links.append(self.INITIAL_URL)
+        index = 0
+
+        # Esegue il crawling di 300 siti link
+        while len(self.links) < self.LIMIT and index < len(self.links):
+            soup = BeautifulSoup(html, 'html.parser')
+            a_list = soup.find_all("a")
+
+            # Carica solo url corretti
+            try: 
+                support = { link.get('href') for link in a_list if link.get('href') and re.search(self.URL_PATTERN, link.get('href')) and re.search("/e/",link.get('href')) }
+            except TypeError:
+                continue
+            new_links = { link for link in support if WebCrawler.get_domain(link) == self.INITIAL_DOMAIN }
+            self.links += new_links
+
+            url = self.links[index]
+            
+
+            print("Crawling: {0}".format(url))
+            print("  Found: {0}".format(len(new_links)))
+            print("  Total: {0}".format(len(self.links)))
+
+            index += 1
+
+            # Un generico errore durante richiesta
+            try:
+                html = requests.get(url).text
+            except Exception:
+                pass
+
+        # Crea i documenti dalla source
+        index = 0
+        for url in self.links:
+            try:
+                html = requests.get(url).text
+            except Exception:
+                continue
+
+            soup = BeautifulSoup(html, 'html.parser')
+            self.create_document_eventbrite(soup,url,index)
+
+            index +=1
+
+
+
+if __name__ == '__main__':
+    
+    wc = WebCrawler(limit=20)
+
+    # Si può passare un url iniziale alle funzioni di run
+    wc.run_meetup()
+    #wc.run_meetup(initial_url="https://www.meetup.com/find/?location=it--bo--Bologna&source=EVENTS")
+
+    wc.run_eventbrite()
+    #wc.run_eventbrite(initial_url="https://www.eventbrite.it/e/biglietti-i-manoscritti-di-qumran-249260694447?aff=ebdssbdestsearch")
