3a6d289e3d8d71c77d892a3f39c2c1ac5be0aef9
Lick1Fonzi <dbianchini21@gmail.com>
2022-06-21 12:29:54 +0200

    pulito preprocessor e aggiunta funzione di ricerca su index

--- src/indexing_whoosh.py
+++ src/indexing_whoosh.py
@@ -1,11 +1,16 @@
+from logging import exception
 import os, os.path
 from whoosh import index
 from whoosh.fields import *
-#from . import Preprocessor
+from whoosh.qparser import MultifieldParser
+from whoosh import scoring
+from gi_progetto.src.preprocessor import preprocess
 
-DOCPATH = os.path.abspath(os.getcwd()) + "\files"
+
+DOCPATH = os.path.abspath(os.getcwd()) + "\Docs"
 SCHEMA = Schema(title=TEXT(stored=True),body=TEXT(stored=True))
 
+#funzione che legge un documento e lo aggiunge all'indice secondo lo schema, per path si intende il nome del documento
 def add_doc(writer, path):
   fileobj = open(path, "rb")
   content = fileobj.read()
@@ -13,59 +18,29 @@ def add_doc(writer, path):
   writer.add_document(title=path, content=content)
 
 
-def get_schema():
-  return Schema(title=TEXT(stored=True),body=TEXT(stored=True))
-
 #oggetto schema serve per definire come viene salvato l'indice 
 #schema = Schema(id=NUMERIC, title=TEXT(stored=True), body=TEXT(stored=True), url=TEXT(stored=True))
-#creo directory che contiene inverted index
+
+#creo directory degli indici, per ogni cartella in Docs creo un indice
 def create_index():
   if not os.path.exists("indexdir"):
       os.mkdir("indexdir")
+  for dir in os.listdir(DOCPATH):
+    ix = index.create_in("indexdir", SCHEMA,indexname=dir)
+    writer = ix.writer
+    for doc in os.listdir(os.path.join(DOCPATH,dir)):
+      add_doc(writer,doc)
+    writer.commit()
 
-#oggetto index a partire dallo schema
-  ix = index.create_in("indexdir", SCHEMA)
-#ix = index.open_dir("indexdir", indexname='')
-  writer = ix.writer()
-  for doc in os.listdir(DOCPATH):
-    add_doc(writer,doc)
-#commit all' index
-  writer.commit()
+def search_index(indexname,keyword):
+  ix = index.open_dir("indexdir",indexname=indexname)
+  q = MultifieldParser(['title', 'body'], schema=ix.schema)
+  keyquery = preprocess(keyword.lower())
+  query = q.parse(keyquery)
+  with ix.searcher(weighting=scoring.TF_IDF()) as searcher:
+        results = searcher.search(query, limit=30)
+        for r in results:
+          print(r)
 
 if __name__ == '__main__':
   create_index()
-
-
-#templates commentati di codici presi da internet 
-'''
-def clean_index(dirname):
-  # Always create the index from scratch
-  ix = index.create_in(dirname, schema=get_schema())
-  writer = ix.writer()
-
-  # Assume we have a function that gathers the filenames of the
-  # documents to be indexed
-  for path in my_docs():
-    add_doc(writer, path)
-
-  writer.commit()
-
-
-def get_schema()
-  return Schema(path=ID(unique=True, stored=True), content=TEXT)
-
-
-def add_doc(writer, path):
-  fileobj = open(path, "rb")
-  content = fileobj.read()
-  fileobj.close()
-  writer.add_document(path=path, content=content)
-
-#da passare al creatore dell'indice docid,titolo,body preprocessato e url da cui deriva
-'''
-
-
-'''
-for p in pagine:
-    writer.add_document(id=p.getId(), title=preProcess(p.getTitolo().lower()), body=preProcess(p.getContenuto().lower()), url=p.getURL())
-'''
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -4,96 +4,64 @@ from nltk.corpus import stopwords
 #nltk.download('book')
 #nltk.download('stopwords')
 #nltk.download('omw-1.4')
-from nltk.stem.porter import PorterStemmer
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.corpus import wordnet as wn
+
+lancaster = LancasterStemmer()
+lemmatizer = nltk.WordNetLemmatizer()
+
+def preprocess(text):
+    tokens = tokenize(text)
+    tokens = removePunctuation(tokens)
+    tokens = removeStopWords(tokens)
+    tokens = lemmatize(tokens)
+    tokens = stem(tokens)
+    return ' '.join(tokens)
 
 def removePunctuation(tokens):
         exclude = set(string.punctuation)
         return [x for x in tokens if x not in exclude]
+
 def removeStopWords(tokens):
     return [x for x in tokens if x not in stopwords.words('english')]
 
-#classe che implementa varie fasi di preprocessing sul testo using python nltk modules
-class Preprocessor():
-    def __init__(self):
-        #self.porter =  PorterStemmer()
-        self.lancaster = LancasterStemmer()
-        self.lemmatizer = nltk.WordNetLemmatizer()
-        self.inv_index = {}
-
-#funzione che accetta un testo e restituisce una lista di token del testo
-    def tokenize(self,text):
-        return nltk.word_tokenize(text.lower())
+def tokenize(text):
+    return nltk.word_tokenize(text.lower())
 
 #Funzione lemmatizzatrice, riceve lista di tokens e restituisce lista di lemmi senza stopwords
-    def lemmatize(self,tokens,lang='english'):
-        return [self.lemmatizer.lemmatize(t) for t in tokens if not t in stopwords.words(lang)]
+def lemmatize(tokens):
+    return [lemmatizer.lemmatize(t) for t in tokens]
 
 #Funzione Stemmer using Lancaster/Porter, restituisce lista con stem di lista di tokens passati come parametro
-    def stem(self,tokens):
-        #stems = [self.porter.stem(t) for t in tokens]
-        return [self.lancaster.stem(t) for t in tokens]
+def stem(tokens):
+    return [lancaster.stem(t) for t in tokens]
 
 #Function that tags every token passed as parameter and returns them in a list of tuples(token,tag)
-    def tag(self,tokens):
-        return nltk.pos_tag(tokens)
+def tag(self,tokens):
+    return nltk.pos_tag(tokens)
     
 #Function disambiguating the terms passed as parameters, determining the right sense using wupalmer similarity
-    def disambiguateTerms(self,terms):
-            for t_i in terms:    # t_i is target term
-                    selSense = None
-                    selScore = 0.0
+def disambiguateTerms(self,terms):
+    for t_i in terms:    # t_i is target term
+            selSense = None
+            selScore = 0.0
     #        print(wn.synsets(t_i,wn.NOUN))
-                    for s_ti in wn.synsets(t_i, wn.NOUN):
-                            score_i = 0.0
-                            for t_j in terms:    # t_j term in t_i's context window
-                                    if (t_i==t_j):
-                                            continue
-                                    bestScore = 0.0
-                                    for s_tj in wn.synsets(t_j, wn.NOUN):
-                                            tempScore = s_ti.wup_similarity(s_tj)
-                                            if (tempScore>bestScore):
-                                                    bestScore=tempScore
-                                    score_i = score_i + bestScore
-                            if (score_i>selScore):
-                                    selScore = score_i
-                                    selSense = s_ti
-                    if (selSense is not None):
-                            print(t_i,": ",selSense,", ",selSense.definition())   
-                            print("Score: ",selScore)
-                    else:
-                            print(t_i,": --")
-
-#Function that creates or updates the inverted index (trie) of the docfile tokenized with a word-based posting list
-#(facilita proximity search, alternativa è docbased con nomefile e occorrenze)
-#non tokenizzato-> da correggere perche per esempio 'parola,' e 'parola' li salva distintamente, ma noi vogliamo che venga tolta la virgola e salvata l'occorrenza per 'parola' correttamente
-    def create_trie(self,docfile):
-        file = open(docfile,'r')
-        trie = {}
-        count = 1
-        for line in file:
-            for word in line.split():
-                token = self.lancaster.stem(self.lemmatizer.lemmatize(''.join(removePunctuation(word.lower()))))
-                print(token)
-                if not token in stopwords.words('english'):
-                    if token not in trie:
-                        trie[token] = [docfile,count]
-                    else:
-                        trie[token].append(count)
-                count += len(word)+1
-        for token in trie:
-            if token not in self.inv_index:
-                self.inv_index[token] = []
-                self.inv_index[token].append(trie[token])
+            for s_ti in wn.synsets(t_i, wn.NOUN):
+                    score_i = 0.0
+                    for t_j in terms:    # t_j term in t_i's context window
+                            if (t_i==t_j):
+                                    continue
+                            bestScore = 0.0
+                            for s_tj in wn.synsets(t_j, wn.NOUN):
+                                    tempScore = s_ti.wup_similarity(s_tj)
+                                    if (tempScore>bestScore):
+                                            bestScore=tempScore
+                            score_i = score_i + bestScore
+                    if (score_i>selScore):
+                            selScore = score_i
+                            selSense = s_ti
+            if (selSense is not None):
+                    print(t_i,": ",selSense,", ",selSense.definition())   
+                    print("Score: ",selScore)
             else:
-                self.inv_index[token].append(trie[token])
-            
-
-if __name__=='__main__':
-    pre = Preprocessor()
-    example = 'text to try the position counting of characters. \n what do i do?'
-    pre.create_trie('File1.txt')
-    pre.create_trie('Doc2.txt')
-    pre.create_trie('Text3.txt')
-    print(pre.inv_index)
+                    print(t_i,": --")
